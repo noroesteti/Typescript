3b03389a2c8a464b394a92f889a907cd7933bd17
Bruno <noroeste.ti@gmail.com>
2023-08-29 23:12:46 -0300

    Connection to external api

--- app/src/controllers/negociacao-controller.ts
+++ app/src/controllers/negociacao-controller.ts
@@ -2,6 +2,7 @@ import { domInjector } from '../decorators/dom-injector.js';
 import { Inspect } from '../decorators/inspect.js';
 import { LogTempoExecucao } from '../decorators/logar-tempo-de-execucao.js';
 import { DiasDaSemana } from '../enums/dias-da-semana.js';
+import { NegociacaoDoDia } from '../interfaces/negociacao-do-dia.js';
 import { Negociacao } from '../models/negociacao.js';
 import { Negociacoes } from '../models/negociacoes.js';
 import { MensagemView } from '../views/mensagem-view.js';
@@ -43,6 +44,27 @@ export class NegociacaoController {
         this.negociacoes.adiciona(negociacao);
         this.limparFormulario();
         this.atualizaView();
+    }
+
+    public importarDados(): void{
+        fetch('http://localhost:8080/dados')
+            .then(res => res.json())
+            .then((dados: NegociacaoDoDia[]) => {
+                return dados.map(dadoDeHoje => {
+                    return new Negociacao(
+                        new Date(), 
+                        dadoDeHoje.vezes, 
+                        dadoDeHoje.montante
+                    )
+                })
+            })   
+            .then(negociacoesDeHoje => {
+                for(let negociacao of negociacoesDeHoje){
+                    this.negociacoes.adiciona(negociacao);
+                } 
+                
+                this.negociacoesView.update(this.negociacoes);
+            });
     }
 
     private ehDiaUtil(data: Date) {
